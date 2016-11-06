f5d830e409b1baa23a6fccd8300abb31bd79d212
Athou <jeremiepanzer@gmail.com>
2016-11-06 16:08:58 +0100

    delay chat message by a random amount

--- main.lua
+++ main.lua
@@ -1,12 +1,21 @@
+local SILENCE_TIME = 10
+local RANDOM_WAIT_TIME_LOW = 3
+local RANDOM_WAIT_TIME_HIGH = 8
+local G_LOW = 5
+local G_HIGH = 12
+
+
 local frame = CreateFrame("Frame", "GGGGG");
 frame:RegisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT");
 
 local last_trigger_time = 0
 local function eventHandler(self, event, ...)
   local _, sender = ...
-  if sender ~= UnitName("player") and (time() - last_trigger_time) > 5 then
-    SendChatMessage(string.rep("G", math.random(5, 12)), "GUILD")
-	last_trigger_time = time()
+  if sender ~= UnitName("player") and (time() - last_trigger_time) > SILENCE_TIME then
+    last_trigger_time = time()
+	C_Timer.After(math.random(RANDOM_WAIT_TIME_LOW, RANDOM_WAIT_TIME_HIGH), function() 
+	  SendChatMessage(string.rep("G", math.random(G_LOW, G_HIGH)), "GUILD") 
+	end)
   end
 end
 frame:SetScript("OnEvent", eventHandler);
