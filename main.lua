54e3e7d8c3f5e84535d18ca2082d49406551dee6
Athou <jeremiepanzer@gmail.com>
2016-11-06 14:55:36 +0100

    don't trigger more than once every 5 seconds and don't trigger for our own events

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
 local frame = CreateFrame("Frame", "GGGGG");
 frame:RegisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT");
 
+local last_trigger_time = 0
 local function eventHandler(self, event, ...)
-  SendChatMessage(string.rep("G", math.random(5, 12)), "GUILD")
+  local _, sender = ...
+  if sender ~= UnitName("player") and (time() - last_trigger_time) > 5 then
+    SendChatMessage(string.rep("G", math.random(5, 12)), "GUILD")
+	last_trigger_time = time()
+  end
 end
 frame:SetScript("OnEvent", eventHandler);
