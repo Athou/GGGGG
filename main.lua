ec940a3d6cd1139ecbe33f4afc2b82e59e88e853
Athou <jeremiepanzer@gmail.com>
2016-11-06 17:40:56 +0100

    fix sender comparison

--- main.lua
+++ main.lua
@@ -11,11 +11,11 @@ frame:RegisterEvent("CHAT_MSG_GUILD_ACHIEVEMENT");
 local last_trigger_time = 0
 local function eventHandler(self, event, ...)
   local _, sender = ...
-  if sender ~= UnitName("player") and (time() - last_trigger_time) > SILENCE_TIME then
+  if sender ~= (UnitName("player") .. "-" .. GetRealmName()) and (time() - last_trigger_time) > SILENCE_TIME then
     last_trigger_time = time()
-	C_Timer.After(math.random(RANDOM_WAIT_TIME_LOW, RANDOM_WAIT_TIME_HIGH), function() 
-	  SendChatMessage(string.rep("G", math.random(G_LOW, G_HIGH)), "GUILD") 
-	end)
+    C_Timer.After(math.random(RANDOM_WAIT_TIME_LOW, RANDOM_WAIT_TIME_HIGH), function() 
+      SendChatMessage(string.rep("G", math.random(G_LOW, G_HIGH)), "GUILD") 
+    end)
   end
 end
 frame:SetScript("OnEvent", eventHandler);
